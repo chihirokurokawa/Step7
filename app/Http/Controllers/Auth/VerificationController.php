cf9b7f148c77f7b1071fe64945930f533fead2d0
chihirokurokawa <Chihiro.Kurokawa.ck@gamil.com>
2022-01-29 10:28:26 +0900

    test

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,6 +26,9 @@ class VerificationController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+   
+    // test
+
 
     /**
      * Create a new controller instance.
